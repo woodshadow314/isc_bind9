a4c2ab8dd35850b7b9b2ec172817e2bdfabee74f
woodshadow314 <woodshadow314@gmail.com>
2021-07-02 22:03:06 +0400

    add view.conf and zones.conf

--- docker/images/isc-bind-9.16/isc-bind.sh
+++ docker/images/isc-bind-9.16/isc-bind.sh
@@ -6,9 +6,9 @@ echo "start of configuration" > /tmp/isc-bind.log
 #     mkdir -p /etc/bind/masters >> /tmp/isc-bind.log
 # fi
 
-# if [ ! -d /etc/bind/slaves ]; then
-#     mkdir -p /etc/bind/slaves >> /tmp/isc-bind.log
-# fi
+if [ ! -d /etc/bind/slaves ]; then
+     mkdir -p /etc/bind/slaves >> /tmp/isc-bind.log
+ fi
 
 chmod -R u=rwx,g=rx /etc/bind/{masters,slaves} >> /tmp/isc-bind.log
 chown -R root:bind /etc/bind/{masters,slaves} >> /tmp/isc-bind.log
